6e6bf32c08e15da406f80ce5ec52bf6a57534e61
EhsanMrf <developermrf@gmail.com>
2024-08-26 23:30:45 +0330

    BookTitleLengthException, BookTitleNullException

---
+++ Train/Domain.Model.Test/Book/BookTitle.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Model.Model.Book;
+using FluentAssertions;
+
+namespace Domain.Model.Test.Book;
+
+internal class BookTitleTest
+{
+    [Fact]
+    public void should_throw_exception_if_title_null()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance(null);
+        };
+
+        courser.Should().Throw<Exception>();
+    }
+}
---
+++ Train/Domain.Model/Model/Book/BookException.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Model.Model.Book;
+
+public class BookTitleNullException() : Exception("Title Null");
+public class BookTitleLengthException() : Exception("Title Length");

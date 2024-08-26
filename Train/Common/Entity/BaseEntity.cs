24f5c82ac98f67bbd0cb3e05a0798ab097d4242e
EhsanMrf <developermrf@gmail.com>
2024-08-26 22:36:46 +0330

    entity base

---
+++ Train/Common/Entity/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Entity
+{
+    public class BaseEntity<T> 
+    {
+        public T Id { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

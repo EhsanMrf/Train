7ddf5d9632f81599d7e9c4bdc3c7c724432645e5
EhsanMrf <developermrf@gmail.com>
2024-08-26 23:01:32 +0330

    create object value for Book

---
+++ Train/Domain.Model/Model/Book/BookTitle.cs
@@ -0,0 +1,36 @@
+﻿using Common.Interface;
+
+namespace Domain.Model.Model.Book;
+
+internal class BookTitle : IObjectValue
+{
+    public string Title { get; private set; }
+
+    public static BookTitle CreateInstance(string title)=> new (title);
+
+    /// <summary>
+    /// for ef core 
+    /// </summary>
+    private BookTitle() { }
+
+    private BookTitle(string title)
+    {
+        SetTitle(title);
+    }
+
+    void SetTitle(string title)
+    {
+        GuardAssessment(title);
+        Title = title;
+    }
+
+    void GuardAssessment(string title)
+    {
+        if (title == null)
+            throw new Exception();
+
+        if (title.Length is 2 or < 2)
+            throw new Exception();
+    }
+
+}

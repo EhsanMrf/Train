9fd69c1af70c36e2136c7cac80ad092ba6f3d2f2
EhsanMrf <developermrf@gmail.com>
2024-08-27 18:53:08 +0330

    test a done

---
+++ Train/Domain.Model.Test/Author/AuthorTest.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Model.Model.Book;
+using FluentAssertions;
+
+namespace Domain.Model.Test.Author;
+
+public class AuthorTest
+{
+
+    [Fact]
+    public void should_throw_exception_if_name_null()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance(null);
+        };
+
+        courser.Should().Throw<BookTitleNullException>();
+    }
+
+    [Fact]
+    public void should_throw_exception_if_name_less_two()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance("A");
+        };
+
+        courser.Should().Throw<BookTitleLengthException>();
+    }
+
+    [Fact]
+    public void should_instance_with_out_exception()
+    {
+        var courser = () =>
+        {
+            var author = new Model.Author.Author(nameof(Model.Author.Author));
+        };
+        courser.Should().NotThrow();
+    }
+}

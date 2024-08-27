7a96ccb64cc16507afd867ab30e880588ce167b8
EhsanMrf <developermrf@gmail.com>
2024-08-27 21:19:21 +0330

    IRepo Author

---
+++ Train/Domain.Model/Model/Author/IRepository/IAuthorCommandRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Response;
+using Domain.Model.Model.Author.Command;
+
+namespace Domain.Model.Model.Author.IRepository;
+
+public interface IAuthorCommandRepository
+{
+    Task Create(AddAuthorCommand command);
+    Task<ServiceResponse<Author>> Update(UpdateAuthorCommand command);
+}
---
+++ Train/Domain.Model/Model/Author/IRepository/IAuthorQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Common.Response;
+using Domain.Model.Model.Author.QueryModel;
+
+namespace Domain.Model.Model.Author.IRepository;
+
+public interface IAuthorQueryRepository
+{
+    Task<ServiceResponse<AuthorQueryModel>> GetById(Guid id);
+    Task<ServiceResponse<DataList<AuthorQueryModel>>> GetList();
+    Task<ServiceResponse<DataList<AuthorBookQueryModel>>> GetAuthorBooksById();
+}

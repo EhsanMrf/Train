de047cc8d53904d1560459b3c40406a29120a450
EhsanMrf <developermrf@gmail.com>
2024-08-27 20:31:31 +0330

    command

---
+++ Train/Domain.Model/Model/Book/Command/AddBookCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Domain.Model.Model.Book.Command;
+
+public record AddBookCommand(BookTitle BookTitle, int PublishYear):IRequest;
---
+++ Train/Domain.Model/Model/Book/Command/UpdateBookCommand.cs
@@ -0,0 +1,11 @@
+﻿using Common.MediatR;
+using Common.Response;
+
+namespace Domain.Model.Model.Book.Command;
+
+public class UpdateBookCommand : RequestMediator<Guid,ServiceResponse<Book>>
+{
+    public BookTitle BookTitle { get; set; } = null!;
+    public int PublishYear { get;  set; }
+    public Guid AuthorId { get; set; }
+}

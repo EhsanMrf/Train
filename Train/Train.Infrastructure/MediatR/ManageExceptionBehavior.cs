9c481ae7ec95c641fd4b6c58d9af13a5731dc521
EhsanMrf <developermrf@gmail.com>
2024-08-27 21:34:59 +0330

    exception behavior

---
+++ Train/Train.Infrastructure/MediatR/ManageExceptionBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Common.Response;
+using MediatR;
+using MediatR.Pipeline;
+
+namespace Infrastructure.MediatR;
+
+public class ManageExceptionBehavior<TRequest, TResponse, TException>
+    : IRequestExceptionHandler<TRequest, TResponse, TException>
+    where TException : Exception where TRequest : IRequest<TResponse> where TResponse : ServiceResponse, new()
+{
+
+    public Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state, CancellationToken cancellationToken)
+    {
+
+        var response = new TResponse
+        {
+            ServiceException = new ServiceException
+            {
+                ErrorMessage = exception.Message,
+                StatusCode = 501
+            }
+        };
+
+        state.SetHandled(response);
+        return Task.CompletedTask;

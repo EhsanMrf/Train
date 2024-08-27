dde6052f3fe3bad2c11e2d49a096a2c937383552
EhsanMrf <developermrf@gmail.com>
2024-08-27 19:05:29 +0330

    add folder service resp

---
+++ Train/Common/Response/DataList.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Response
+{
+    public class DataList<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Page { get; set; }
+        public int TotalPage { get; set; }
+        public int TotalCount { get; set; }
+
+        public DataList(IEnumerable<T> items, int totalCount, int page, int pageSize)
+        {
+            Page = page;
+            TotalPage = (int)Math.Ceiling(totalCount / (double)pageSize);
+            Items = items;
+            TotalCount = totalCount;
+        }
+    }
+}
---
+++ Train/Common/Response/ServiceResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace Common.Response;
+
+public class ServiceResponse
+{
+}

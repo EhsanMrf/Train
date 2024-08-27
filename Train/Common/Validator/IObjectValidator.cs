e9981b0442cc7f3422d882c65d5bf3800ae7d108
EhsanMrf <developermrf@gmail.com>
2024-08-27 18:22:12 +0330

    step Builder and validator

---
+++ Train/Common/Validator/IObjectValidator.cs
@@ -0,0 +1,21 @@
+﻿using Common.Exception;
+
+namespace Common.Validator;
+
+public interface IObjectValidator
+{
+    public ISingleObjectValidator RuleFor(object data);
+    public IMultiObjectValidator RuleFor(params object[] objects);
+    public IObjectValidator Must<T>(T ob, Func<T, bool> expression, BaseException exception);
+    IObjectValidator Must<T>(T ob, Func<T, bool> expression, BaseException exception, bool defaultReturn);
+
+}
+public interface ISingleObjectValidator
+{
+    public IObjectValidator NotNullOrEmpty(BaseException exception);
+}
+
+public interface IMultiObjectValidator
+{
+    public IMultiObjectValidator NotNullOrEmptyList(BaseException exception);
+}
